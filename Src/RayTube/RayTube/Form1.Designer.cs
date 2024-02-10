8231cdd980306c74ba13b82a7f22d1b7953afc94
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-02-10 22:52:15 +0100

    Add about

--- Src/RayTube/RayTube/Form1.Designer.cs
+++ Src/RayTube/RayTube/Form1.Designer.cs
@@ -56,6 +56,7 @@
             this.TransparencyKey = System.Drawing.Color.Magenta;
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
 
         }
